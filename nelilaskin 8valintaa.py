cbb574aad3a20b1047947f70d1363c9891c1803b
Hannu Pham <Hannu@Hannus-MacBook-Air.local>
2023-04-18 11:25:15 +0300

    Haloo onnistuko

--- nelilaskin 8valintaa.py
+++ nelilaskin 8valintaa.py
@@ -1,39 +1,29 @@
 import math
+#if-elif-else-rakenteen käyttäminen
+luku1 = int(input("Anna ensimmäinen luku: "))
+luku2 = int(input("Anna toinen luku: "))
 
 while True:
-    luku1 = int(input("Anna ensimmäinen luku: "))
-    luku2 = int(input("Anna toinen luku: "))
-
-    print("(1) +")
-    print("(2) -")
-    print("(3) *")
-    print("(4) /")
-    print("(5) sin(luku1/luku2)")
-    print("(6) cos(luku1/luku2)")
-    print("(7) Vaihda luvut")
-    print("(8) Lopeta")
-    print("Valitut luvut:", luku1, luku2)
-
+    print("(1) +\n(2) -\n(3) *\n(4) /\n(5)sin(luku1/luku2)\n\
+(6)cos(luku1/luku2)\n(7)Vaihda luvut\n(8)Lopeta")
+    print("Valitut luvut:",luku1,luku2)
     valinta = int(input("Tee valinta (1-8): "))
-
     if valinta == 1:
-        tulos = luku1 + luku2
+        print("Tulos on:", luku1+luku2)
     elif valinta == 2:
-        tulos = luku1 - luku2
+        print("Tulos on:", luku1-luku2)
     elif valinta == 3:
-        tulos = luku1 * luku2
+        print("Tulos on:", luku1*luku2)
     elif valinta == 4:
-        tulos = luku1 / luku2
+        print("Tulos on:", luku1/luku2)
     elif valinta == 5:
-        tulos = math.sin(luku1/luku2)
+        print("Tulos on:", math.sin(luku1/luku2))
     elif valinta == 6:
-        tulos = math.cos(luku1/luku2)
+        print("Tulos on:", math.cos(luku1/luku2))
     elif valinta == 7:
-        continue
+        luku1 = int(input("Anna uusi ensimmäinen luku: "))
+        luku2 = int(input("Anna uusi toinen luku: "))
     elif valinta == 8:
         break
     else:
-        print("Yritä uudelleen.")
-        continue
-
-    print("Tulos: ", tulos)
+        print("Valintaa ei tunnistettu.")
--- nelilaskin chapter 11.py
+++ nelilaskin chapter 11.py
@@ -1,60 +1,29 @@
 import math
-
-def get_integer_input(prompt):
-    while True:
-        try:
-            value = int(input(prompt))
-            return value
-        except ValueError:
-            print("Virheellinen syöte!")
-
-num1 = get_integer_input("Anna luku: ")
-num2 = get_integer_input("Anna luku: ")
+#if-elif-else-rakenteen käyttäminen
+luku1 = int(input("Anna ensimmäinen luku: "))
+luku2 = int(input("Anna toinen luku: "))
 
 while True:
-    print("(1) +")
-    print("(2) -")
-    print("(3) *")
-    print("(4) /")
-    print("(5)sin(luku1/luku2)")
-    print("(6)cos(luku1/luku2)")
-    print("(7)Vaihda luvut")
-    print("(8)Lopeta")
-    print("Valitut luvut:", num1 , num2)
-
-    choice = get_integer_input("Tee valinta (1-8): ")
-
-    if choice == 1:
-        result = num1 + num2
-        print("Tulos on: ", result)
-    elif choice == 2:
-        result = num1 - num2
-        print("Tulos on: ", result)
-    elif choice == 3:
-        result = num1 * num2
-        print("Tulos on: ", result)
-    elif choice == 4:
-        if num2 == 0:
-            print("Virheellinen syöte! Et voi jakaa luvulla 0.")
-        else:
-            result = num1 / num2
-            print("Tulos on: ", result)
-    elif choice == 5:
-        try:
-            result = math.sin(num1 / num2)
-            print("Tulos on: ", result)
-        except ZeroDivisionError:
-            print("Virheellinen syöte! Et voi jakaa luvulla 0.")
-    elif choice == 6:
-        try:
-            result = math.cos(num1 / num2)
-            print("Tulos on: ", result)
-        except ZeroDivisionError:
-            print("Virheellinen syöte! Et voi jakaa luvulla 0.")
-    elif choice == 7:
-        num1 = get_integer_input("Anna luku: ")
-        num2 = get_integer_input("Anna luku: ")
-    elif choice == 8:
+    print("(1) +\n(2) -\n(3) *\n(4) /\n(5)sin(luku1/luku2)\n\
+(6)cos(luku1/luku2)\n(7)Vaihda luvut\n(8)Lopeta")
+    print("Valitut luvut:",luku1,luku2)
+    valinta = int(input("Tee valinta (1-8): "))
+    if valinta == 1:
+        print("Tulos on:", luku1+luku2)
+    elif valinta == 2:
+        print("Tulos on:", luku1-luku2)
+    elif valinta == 3:
+        print("Tulos on:", luku1*luku2)
+    elif valinta == 4:
+        print("Tulos on:", luku1/luku2)
+    elif valinta == 5:
+        print("Tulos on:", math.sin(luku1/luku2))
+    elif valinta == 6:
+        print("Tulos on:", math.cos(luku1/luku2))
+    elif valinta == 7:
+        luku1 = int(input("Anna uusi ensimmäinen luku: "))
+        luku2 = int(input("Anna uusi toinen luku: "))
+    elif valinta == 8:
         break
     else:
-        print("Virheellinen valinta!")
+        print("Valintaa ei tunnistettu.")
